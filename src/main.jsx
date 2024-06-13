0bd28057c7d4dd3975466e6e1e6791e09fb7a469
NalinaRanjan <nalinanandu995@gmail.com>
2024-06-13 12:30:37 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
-import StoreContextProvider from './context/storeContext.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import StoreContextProvider from './context/storeContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
@@ -11,7 +11,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <App />
     </StoreContextProvider>
   </BrowserRouter>
-
-)
-
-
+);
