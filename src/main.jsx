6a1a68444166d5e0f467cca7b1a0f7b943740a9b
NalinaRanjan <nalinanandu995@gmail.com>
2024-06-13 15:40:52 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
+//import { BrowserRouter } from 'react-router-dom';
 import StoreContextProvider from './context/storeContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+ // <BrowserRouter>
     <StoreContextProvider>
       <App />
     </StoreContextProvider>
-  </BrowserRouter>
+//  </BrowserRouter>
 );
