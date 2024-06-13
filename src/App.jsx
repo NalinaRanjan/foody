26567732685ab55affe85b2b4d4d16fff0709c68
NalinaRanjan <nalinanandu995@gmail.com>
2024-06-13 15:40:34 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import Navbar from './components/Navbar/Navbar'
-import {Routes, Route  } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home'
 import Cart from './pages/Cart/Cart'
 import PlaceOrder from './pages/Place Order/PlaceOrder'
@@ -17,12 +17,13 @@ const[showPopUp , setShowPopUp] = useState(false)
     {showPopUp ? <LoginPopUp setShowPopUp={setShowPopUp}/> : <></>}
       <div className='app'>
         <Navbar showPopUp={showPopUp} setShowPopUp={setShowPopUp} />
-        
+        <Router basename="/foody">
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/Cart' element={<Cart/>} />
           <Route path='/PlaceOrder' element={<PlaceOrder />} />
         </Routes>
+          </Router>
       </div>
       <Footer />
     </>
